b99ab2edd99a650424e7d5b9a4ed9491c6bdc151
apoorvcodes <tofu.develops@gmail.com>
2022-11-19 00:12:06 +0530

    feat: clean up

---
+++ eviate.config.ts
@@ -0,0 +1,10 @@
+import { config } from 'eviate';
+import {PORT, HOST, DEBUG} from "./src/constants"
+
+const config: config = {
+    port: PORT,
+    hostname: HOST,
+    debug: DEBUG
+};
+
+export default config;

f68f2f4f94722dd460b3375540f09a0bb882d8c7
apoorvcodes <tofu.develops@gmail.com>
2022-11-21 20:25:52 +0530

    feat: removing rust for simulations

--- simulations/target/debug/deps/simulations-ed5ec68dc3631f71.d
+++
@@ -1,5 +0,0 @@
-/Users/apoorv/Dont-touch/rose/simulations/target/debug/deps/simulations-ed5ec68dc3631f71: src/main.rs
-
-/Users/apoorv/Dont-touch/rose/simulations/target/debug/deps/simulations-ed5ec68dc3631f71.d: src/main.rs
-
-src/main.rs:
--- simulations/target/debug/simulations.d
+++
@@ -1,1 +0,0 @@
-/Users/apoorv/Dont-touch/rose/simulations/target/debug/simulations: /Users/apoorv/Dont-touch/rose/simulations/src/main.rs

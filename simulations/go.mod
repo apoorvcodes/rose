e41a5ab98eaf45fbc2e3bd1859f152e4e4360fd5
apoorvcodes <tofu.develops@gmail.com>
2022-11-21 23:22:30 +0530

    feat: golang setup

---
+++ simulations/go.mod
@@ -0,0 +1,5 @@
+module simulations
+
+go 1.19
+
+require github.com/joho/godotenv v1.4.0 // indirect
---
+++ simulations/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+ err := godotenv.Load();
+
+ if err != nil {
+   fmt.Print("ENV not loaded might be production")
+ }
+
+}

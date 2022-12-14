d57849fdef04c35daae8ae5c3a828fc21bb8e973
Apoorv <tofu.develops@gmail.com>
2022-12-14 18:50:54 +0530

    feat: reverting eviate (#5)

---
+++ src/routes/roses/information.ts
@@ -0,0 +1,12 @@
+import { Context, EviateResponse } from "eviate";
+import { FileRouteInterface } from "src/interfaces/fileRouter";
+
+export const route: FileRouteInterface = {
+  method: "GET",
+  run: (_: Context): EviateResponse => {
+    return {
+      text: "This for information",
+      status: 200,
+    };
+  },
+};

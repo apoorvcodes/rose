46d83c02c40787f02d0e6aa65206c2baf9370077
apoorvcodes <tofu.develops@gmail.com>
2022-12-14 19:03:45 +0530

    feat: mongoose setp

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,8 @@ const app: Engine = new Engine();
 app.get("/", (ctx: Context) => {
   console.log(ctx.headers);
   return {
-    json: {
-      Name: "Apoorv",
-    },
+    interface: ctx.file("src/static/Home.html"),
+    status: 200
   };
 });
 

e840ad156082a4e2d8d42c43ad9b73fe909d04f4
apoorvcodes <tofu.develops@gmail.com>
2022-12-15 11:50:49 +0530

    feat: fix stupid bug

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import { Context, Engine } from "eviate";
 const app: Engine = new Engine();
 
 app.get("/", (ctx: Context) => {
-  console.log(ctx.headers);
   return {
     interface: ctx.file("src/static/Home.html"),
     status: 200,
